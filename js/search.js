9fdbeb6e58650dca62005991a2f2c9629bd87ae9
cMardc <merd.yedek@gmail.com>
2023-08-28 17:38:16 +0400

    Search Fix

--- js/search.js
+++ js/search.js
@@ -6,6 +6,9 @@ const urlParams = new URLSearchParams(queryString);
 if(urlParams.has('search')) {
     console.log(urlParams.get('search'));
     let term = urlParams.get('search').toLowerCase();
+    if(term === '') {
+        window.location.href = "https://cmardc.github.io/cMardc/html/search.html";
+    }
     projects = document.getElementsByClassName('code');
 
     for (let index = 0; index < projects.length; index++) {
